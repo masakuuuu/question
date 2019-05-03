6cfa7f102c6ee32491b5e47c4fc2fd0fd3480683
MasaKuuuu <masaki_kuyama@yahoo.co.jp>
2019-05-03 13:51:31 +0900

    jQueryバージョンアップ

--- resources/views/components/includeCommon.blade.php
+++ resources/views/components/includeCommon.blade.php
@@ -6,7 +6,7 @@
 <script src="js/uikit-icons.min.js"></script>
 
 <!-- JavaScript -->
-<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-3.4.1.min.js"></script>
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
