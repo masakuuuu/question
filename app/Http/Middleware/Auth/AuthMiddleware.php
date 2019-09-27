72154cfd45a0df703772722da7e78207a5b9f785
MasaKuuuu <masaki_kuyama@yahoo.co.jp>
2019-09-27 20:44:17 +0900

    AuthMiddleware の追加

---
+++ app/Http/Middleware/Auth/AuthMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware\Auth;
+
+use Closure;
+
+class AuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // requestしてきたアクション名をセッションにセット
+        session(array(
+            'redirectUrl' => $_SERVER['REDIRECT_URL'],
+        ));
+
+        // URLハッシュがある場合はセットする
+        if($request->url_hash){
+            session(array(
+                'url_hash'    => $request->url_hash,
+            ));
+         }
+
+        // TwitterのOAuth
+        $twitterUserId = session('twitter_user_id');
+        $name = session('name');
+        $screenName = session('screen_name');
+        $profileImageUrlHttps = session('profile_image_url_https');
+
+        // twitter認証できていない場合は認証させる
+        if (!$twitterUserId && !$name && !$screenName && !$profileImageUrlHttps) {
+            return redirect('/OAuth');
+        }
+        return $next($request);
+    }
+}
